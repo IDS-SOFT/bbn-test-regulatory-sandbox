1740a26b9651dabcda27abc5d7cc2d2fdad3d9fd
PriyankaReddy-21 <125461903+PriyankaReddy-21@users.noreply.github.com>
2023-09-20 16:38:48 +0530

    Add files via upload

---
+++ scripts/deploy.ts
@@ -0,0 +1,21 @@
+
+import { ethers } from "hardhat";
+
+const name = "MySandBox";
+const address = "0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199";
+
+async function main() {
+
+  const deploy_contract = await ethers.deployContract("RegulatorySandbox", [name, address]);
+
+  await deploy_contract.waitForDeployment();
+
+  console.log("RegulatorySandbox is deployed to : ",await deploy_contract.getAddress());
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
